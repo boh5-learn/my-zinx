51551f7edb6a011b51a9009d07c7257e291df484
boh5 <bohuang1994@gmail.com>
2022-11-25 15:36:53 +0800

    :sparkles: Implement Request

---
+++ ziface/irequest.go
@@ -0,0 +1,9 @@
+package ziface
+
+// IRequest 把客户端请求的 conn 和 data 包装到一个 Request 中
+type IRequest interface {
+	// GetConnection 得到当前连接
+	GetConnection() IConnection
+	// GetData 得到请求的数据
+	GetData() []byte
+}
